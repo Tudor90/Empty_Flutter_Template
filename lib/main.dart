d53deb8972ec14bddc22e7b77137002e3f26597e
Tudor <tudor.iordache@thinslices.com>
2023-02-08 12:38:24 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:empty_project/pages/login_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Project',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const LoginPage(title: 'Login'),
+    );
+  }
+}
+
